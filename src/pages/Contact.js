0cd9c875d075dc0622704b18857e7ab416d94e31
Philip Tootill <philip-tootill@softwareimaging.com>
2020-06-04 16:29:50 +0100

    Contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 function Contact() {
   return (
-    <div>
-      Contact
+    <div className="page-content">
+      <p className="page-title">Contact</p>
+      <div className="page-section">
+        <p>If you have questions about any of my games, feel free to get in touch with me.</p>
+        <p>Twitter: @WSGameDesign</p>
+      </div>
     </div>
   );
 }
