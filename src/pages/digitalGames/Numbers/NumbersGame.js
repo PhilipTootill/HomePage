208c546178f1e365eaf5d51a9d04d08cd6a74ca7
Philip Tootill <philip-tootill@softwareimaging.com>
2020-06-09 10:36:42 +0100

    Refactoring to use a class component

--- src/pages/digitalGames/Numbers/NumbersGame.js
+++ src/pages/digitalGames/Numbers/NumbersGame.js
@@ -1,117 +1,132 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './Numbers.css';
 
-function NumbersGame() {
-  var numbersArray = [];
-  for (var i = 0; i < 4; i++) {
-    numbersArray.push(RandomInteger(9, 1, 1));
-  }
+class NumbersGame extends React.Component {
+    constructor(props) {
+        super(props);
 
-  numbersArray.push(RandomInteger(25, 10, 5));
-  numbersArray.push(RandomInteger(100, 25, 25));
+        var numbersArray = [];
+        for (var i = 0; i < 4; i++) {
+            numbersArray.push(this.randomInteger(9, 1, 1));
+        }
 
-  const operations = ["+", "-", "X", "/"];
+        numbersArray.push(this.randomInteger(25, 10, 5));
+        numbersArray.push(this.randomInteger(100, 25, 25));
 
-  const [numbers, setNumbers] = useState(numbersArray);
-  const [target, setTarget] = useState(RandomInteger(50, 21, 1));
-  const [score, setScore] = useState(0);
-  const [highlightedOperation, setHighlightedOperation] = useState();
-  const [highlightedIndex, setHighlightedIndex] = useState();
-
-  function handleNumberClick(index) {
-    if (highlightedIndex == null) {
-      setHighlightedIndex(index);
-    } else if (index == highlightedIndex) {
-      setHighlightedIndex(null);
-    } else {
-      var firstIndex = Math.min(index, highlightedIndex);
-      var secondIndex = Math.max(index, highlightedIndex);
-      var firstNumber = Math.max(numbers[firstIndex], numbers[secondIndex]);
-      var secondNumber = Math.min(numbers[firstIndex], numbers[secondIndex]);
-
-      var createdNumber;
-
-      switch(highlightedOperation) {
-        case "+":
-          createdNumber = firstNumber + secondNumber;
-          break;
-        case "-":
-          createdNumber = firstNumber - secondNumber;
-          break;
-        case "X":
-          createdNumber = firstNumber * secondNumber;
-          break;
-        case "/":
-          if (firstNumber % secondNumber == 0) {
-            createdNumber = firstNumber / secondNumber;
-          } else {
-            return;
-          }
-          break;
-        default:
-          return;
-      }
-
-      var newNumbersArray = numbers.splice(0, 6);
-      newNumbersArray.splice(firstIndex, 1);
-      newNumbersArray.splice(secondIndex - 1, 1); //secondIndex has shifted by removal.
-
-      newNumbersArray.splice(index, 0, createdNumber);
-      newNumbersArray.push(RandomInteger(9, 1, 1));
-
-      setNumbers(newNumbersArray);
-      setHighlightedIndex(null);
-      setHighlightedOperation(null);
-
-      if (createdNumber == target) {
-        setScore(score + 1);
-        setTarget(RandomInteger(50, 21, 1));
-      }
+        this.state = {
+            numbers: numbersArray,
+            target: this.randomInteger(50, 21, 1),
+            score: 0,
+            movesRemaining: 6,
+            highlightedOperation: null,
+            highlightedIndex: null
+        }
     }
-  }
 
-  function handleOperationClick(operation) {
-    setHighlightedOperation(operation);
-  }
+    operations = ["+", "-", "X", "/"];
 
-  return (
-    <div>
-      <div className="number-box-container">
-        {numbers.map((number, index) => 
-          <div className="number-box" onClick={function() {handleNumberClick(index)}} 
-               highlighted={index == highlightedIndex ? "true" : null}>
-            {number}
-          </div>
-        )}
-      </div>
-      <div className="operation-box-container">
-        {operations.map((operation) => 
-          <div className="operation-box" onClick={function() {handleOperationClick(operation)}} 
-               highlighted={operation == highlightedOperation ? "true" : null}>
-            {operation}
-          </div>
-        )}
-      </div>
-      <p>Target is: {target}</p>
-    </div>
-  );
-}
+    /*
+    * Return a random multiple of an integer, up to a set maximum.
+    */
+    randomInteger(max, min, multiple) {
+        //Scale everything down by the multiplier.
+        var scaledMax = Math.floor(max / multiple);
+        var scaledMin = Math.ceil(min / multiple);
+        var range = scaledMax - scaledMin;
 
-/*
- * Return a random multiple of an integer, up to a set maximum.
- */
-function RandomInteger(max, min, multiple) {
-  var range = max - min + 1;
-  var multipleRange = Math.floor(range / multiple);
-  var multipleLowerLimit = Math.floor(min / multiple) - 1;
+        var random;
+        random = Math.random(); //Between 0 and 1.
+        random = random * range; // Between 0 and range.
+        random = Math.round(random); //Integer between 0 and range
+        random = random + scaledMin; //Integer between scaledMin and scaledMax
+        random = random * multiple; //Multiple between min and max.
 
-  var random = Math.random(); //Between 0 and 1.
-  random = random * multipleRange; // Between 0 and multipleRange.
-  random = Math.ceil(random); //Integer between 1 and multipleRange
-  random = random + multipleLowerLimit;
-  random = random * multiple; //Definitely a multiple.
+        return random;
+    }
 
-  return random;
+    handleNumberClick(index) {
+        if (this.state.highlightedIndex == null) {
+            this.setState({highlightedIndex: index});
+        } else if (index == this.state.highlightedIndex) {
+            this.setState({highlightedIndex: null});
+        } else {
+            var firstIndex = Math.min(index, this.state.highlightedIndex);
+            var secondIndex = Math.max(index, this.state.highlightedIndex);
+            var firstNumber = Math.max(this.state.numbers[firstIndex], this.state.numbers[secondIndex]);
+            var secondNumber = Math.min(this.state.numbers[firstIndex], this.state.numbers[secondIndex]);
+    
+            var createdNumber;
+    
+            switch (this.state.highlightedOperation) {
+                case "+":
+                    createdNumber = firstNumber + secondNumber;
+                    break;
+                case "-":
+                    createdNumber = firstNumber - secondNumber;
+                    break;
+                case "X":
+                    createdNumber = firstNumber * secondNumber;
+                    break;
+                case "/":
+                    if (firstNumber % secondNumber == 0) {
+                        createdNumber = firstNumber / secondNumber;
+                    } else {
+                        return;
+                    }
+                    break;
+                default:
+                    return;
+            }
+    
+            var newNumbersArray = this.state.numbers.splice(0, 6);
+            newNumbersArray.splice(firstIndex, 1);
+            newNumbersArray.splice(secondIndex - 1, 1); //secondIndex has shifted by removal.
+    
+            newNumbersArray.splice(index, 0, createdNumber);
+            newNumbersArray.push(this.randomInteger(9, 1, 1));
+    
+            this.setState({
+                numbers: newNumbersArray,
+                highlightedIndex: null,
+                highlightedOperation: null
+            });
+    
+            if (createdNumber == this.state.target) {
+                this.setState({
+                    score: this.state.score + 1,
+                    target: this.randomInteger(50, 21, 1)
+                });
+            }
+        }
+    }
+
+    handleOperationClick(operation) {
+        this.setState({highlightedOperation: operation});
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="number-box-container">
+                    {this.state.numbers.map((number, index) =>
+                        <div className="number-box" onClick={() => { this.handleNumberClick(index) }}
+                            highlighted={index == this.state.highlightedIndex ? "true" : null}>
+                            {number}
+                        </div>
+                    )}
+                </div>
+                <div className="operation-box-container">
+                    {this.operations.map((operation) =>
+                        <div className="operation-box" onClick={() => { this.handleOperationClick(operation) }}
+                            highlighted={operation == this.state.highlightedOperation ? "true" : null}>
+                            {operation}
+                        </div>
+                    )}
+                </div>
+                <p>Target is: {this.state.target}</p>
+            </div>
+        );
+    }
 }
 
 export default NumbersGame;
