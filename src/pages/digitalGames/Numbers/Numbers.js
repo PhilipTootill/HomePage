f2bb58e07130f24999d4b766d2fb1abde6de625c
Philip Tootill <philip-tootill@softwareimaging.com>
2020-06-11 13:32:46 +0100

    Basic markdown file is displayed

--- src/pages/digitalGames/Numbers/Numbers.js
+++ src/pages/digitalGames/Numbers/Numbers.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
+import ReactMarkdown from "react-markdown";
 import NumbersGame from './NumbersGame';
+import NumbersMarkdown from './Numbers.md'
 
 function Numbers() {
+  const [markdown, setMarkdown] = useState("");
+  fetch(NumbersMarkdown).then(res => res.text()).then(text => setMarkdown(text));
+
+
   return (
     <div>
       <p className="page-title">Numbers</p>
       <div>
         <NumbersGame/>
+      </div>
+      <div className="page-section">
+        <ReactMarkdown source={markdown} />
       </div>
     </div>
   );
