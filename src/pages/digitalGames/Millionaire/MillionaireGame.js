71d57d5f7d38c6b2f738c6d96e95c26049de5563
Philip Tootill <philip-tootill@softwareimaging.com>
2020-06-30 08:32:13 +0100

    Basic state added

--- src/pages/digitalGames/Millionaire/MillionaireGame.js
+++ src/pages/digitalGames/Millionaire/MillionaireGame.js
@@ -15,22 +15,23 @@ const pointsList = [
     "13 points",
     "16 points",
     "20 points"
-]
-    
-const question = "Which of these objects is the biggest?";
-
-const answerList = [
-    "An Elephant",
-    "The Moon",
-    "A Peanut",
-    "A Kettle"
 ];
-
-const message = "This is the message box!";
 
 class MillionaireGame extends React.Component {
     constructor(props) {
         super(props);
+
+        this.questions = props.answers;
+
+        this.state = {
+            message: "This is the message box!",
+            questionIndex: 0,
+            fiftyFiftyAvailable: true,
+            firstPhoneAvailable: true,
+            secondPhoneAvailable: true,
+            walkedAway: false,
+            highlightedAnswer: null
+        }
     }
 
     render() {
@@ -38,23 +39,28 @@ class MillionaireGame extends React.Component {
             <div className="millionaire-game-container">
                 <div className="millionaire-main-panel">
                     <div className="millionaire-question-container">
-                        {question}
+                        {this.questions[this.state.questionIndex].question}
                     </div>
                     <div className="millionaire-answers-container">
-                        {answerList.map((answer, answerIndex) =>
+                        {this.questions[this.state.questionIndex].answers.map((answer, answerIndex) =>
                             <div className="millionaire-answer">
-                                {answerList[answerIndex]}
+                                {answer}
                             </div>
                         )}
                     </div>
                     <div className="millionaire-message-box">
-                        {message}
+                        {this.state.message}
                     </div>
                 </div>
                 <div className="millionaire-side-panel">
                     <div className="millionaire-score-tracker">
                         {pointsList.map((points, pointsIndex) =>
-                            <div className="millionaire-points" checkpoint={pointsIndex % 4 === 3 ? "true" : null}>{points}</div>
+                            <div 
+                                className="millionaire-points" 
+                                checkpoint={pointsIndex % 4 === 3 ? "true" : null}
+                                currentscore={this.state.questionIndex - 1 === pointsIndex ? "true" : null}>
+                                {points}
+                            </div>
                         )}
                     </div>
                     <div className="millionaire-lifelines">
