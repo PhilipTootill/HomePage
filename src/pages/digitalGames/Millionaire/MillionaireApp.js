61cedc1fc1a542749c80b3f55b4e5749975045f3
Philip Tootill <philip-tootill@softwareimaging.com>
2020-06-30 08:22:29 +0100

    Import test questions from external file

--- src/pages/digitalGames/Millionaire/MillionaireApp.js
+++ src/pages/digitalGames/Millionaire/MillionaireApp.js
@@ -1,18 +1,10 @@
 import React, { useState } from 'react';
 import './Millionaire.css';
 import MillionaireGame from './MillionaireGame';
+import { testPuzzle } from './MillionaireQuestions';
 
 function MillionaireApp() {
     const [puzzle, setPuzzle] = useState(null);
-
-    const puzzle1 = [
-        ["Link", "Truss", "Annex", "Correlate"],
-        ["Samus", "Ratchet", "Steve", "Forty Seven"],
-        ["Fox", "Panther", "Zebra", "Snake"],
-        ["Papadam", "Seventy", "Marocain", "Gabelle"],
-        ["Nine", "Twenty Five", "Forty Nine", "Sixty Four"]
-    ];
-    
     
     const buttonClick = (puzzle) => {
         setPuzzle(puzzle);
@@ -27,7 +19,7 @@ function MillionaireApp() {
             <p className="millionaire-intro-title">Who Wants To Win 20 Points?</p>
             <p className="millionaire-intro-text">When your whole team can see your screen and is ready, click start to begin!</p>
             <div className="millionaire-intro-buttons">
-                <button className="millionaire-start-button" onClick={()=>{buttonClick(puzzle1)}}>Start</button>
+                <button className="millionaire-start-button" onClick={()=>{buttonClick(testPuzzle)}}>Test</button>
             </div> 
         </div>
     }
---
+++ src/pages/digitalGames/Millionaire/MillionaireQuestions.js
@@ -0,0 +1,132 @@
+export const testPuzzle = [
+    {
+        question: "Question 1.",
+        answers: [
+            "One",
+            "Two",
+            "Three",
+            "Correct"
+        ],
+        correctAnswer: "Correct"
+    },
+    {
+        question: "Question 2.",
+        answers: [
+            "One",
+            "Two",
+            "Three",
+            "Correct"
+        ],
+        correctAnswer: "Correct"
+    },
+    {
+        question: "Question 3.",
+        answers: [
+            "One",
+            "Two",
+            "Three",
+            "Correct"
+        ],
+        correctAnswer: "Correct"
+    },
+    {
+        question: "Question 4.",
+        answers: [
+            "One",
+            "Two",
+            "Three",
+            "Correct"
+        ],
+        correctAnswer: "Correct"
+    },
+    {
+        question: "Question 4.",
+        answers: [
+            "One",
+            "Two",
+            "Three",
+            "Correct"
+        ],
+        correctAnswer: "Correct"
+    },
+    {
+        question: "Question 5.",
+        answers: [
+            "One",
+            "Two",
+            "Three",
+            "Correct"
+        ],
+        correctAnswer: "Correct"
+    },
+    {
+        question: "Question 6.",
+        answers: [
+            "One",
+            "Two",
+            "Three",
+            "Correct"
+        ],
+        correctAnswer: "Correct"
+    },
+    {
+        question: "Question 7.",
+        answers: [
+            "One",
+            "Two",
+            "Three",
+            "Correct"
+        ],
+        correctAnswer: "Correct"
+    },
+    {
+        question: "Question 8.",
+        answers: [
+            "One",
+            "Two",
+            "Three",
+            "Correct"
+        ],
+        correctAnswer: "Correct"
+    },
+    {
+        question: "Question 9.",
+        answers: [
+            "One",
+            "Two",
+            "Three",
+            "Correct"
+        ],
+        correctAnswer: "Correct"
+    },
+    {
+        question: "Question 10.",
+        answers: [
+            "One",
+            "Two",
+            "Three",
+            "Correct"
+        ],
+        correctAnswer: "Correct"
+    },
+    {
+        question: "Question 11.",
+        answers: [
+            "One",
+            "Two",
+            "Three",
+            "Correct"
+        ],
+        correctAnswer: "Correct"
+    },
+    {
+        question: "Question 12.",
+        answers: [
+            "One",
+            "Two",
+            "Three",
+            "Correct"
+        ],
+        correctAnswer: "Correct"
+    }
+];
